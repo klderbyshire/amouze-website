f3cf352fbf0101d422cc99262042228042c26dc7
karen derbyshire <karenlinzi2605@gmail.com>
2023-01-27 15:30:38 +0000

    careers page asterisk updated

--- Homepage/homepage.js
+++ Homepage/homepage.js
@@ -1,3 +1,10 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-right")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
 function checkBox() {
   const checkbox = document.getElementById("terms-and-conditions");
   const submit = document.getElementById("button-blue");
